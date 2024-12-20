25521320f63f2608053184fc4d960e7af12f7cab
Sayan Sengupta <subhosen16@gmail.com>
2024-12-20 13:21:05 +0530

    Added the problem statement to Beautiful Strings

--- dsa-practice-problems/src/basic_problems/BeautifulString.java
+++ dsa-practice-problems/src/basic_problems/BeautifulString.java
@@ -1,5 +1,16 @@
 package basic_problems;
 
+/***
+ * Ninja has been given a binary string ‘STR’ containing either ‘0’ or ‘1’.
+ * A binary string is called beautiful if it contains alternating 0s and 1s.
+ *
+ * For Example:‘0101’, ‘1010’, ‘101’, ‘010’ are beautiful strings.
+ *
+ * He wants to make ‘STR’ beautiful by performing some operations on it.
+ * In one operation, Ninja can convert ‘0’ into ‘1’ or vice versa.
+ *
+ * Your task is to determine the minimum number of operations Ninja should perform to make ‘STR’ beautiful.
+ */
 public class BeautifulString {
     public static int makeBeautiful(String str) {
         int withoutModifyingFirstCharacter = modify(str, str.charAt(0));
